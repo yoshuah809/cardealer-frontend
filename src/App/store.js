cfc4b9e22e806ebb04f7dc5167be4ede25b25c57
Josue Garcia <yoshuah809@gmail.com>
2021-12-10 07:59:12 -0600

    Created Actions, reducer and constants for userlogin

--- src/App/store.js
+++ src/App/store.js
@@ -3,21 +3,28 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { vehicleListReducer, vehicleDetailsReducer } from '../Components/reducers/VehicleReducer'
 import { CartReducer } from '../Components/reducers/CartReducer'
+import { userLoginReducer } from '../Components/reducers/UserReducer'
 
 const reducer = combineReducers({
     vehicleList: vehicleListReducer,
     vehicleDetails: vehicleDetailsReducer,
     cart: CartReducer,
+    userlogin: userLoginReducer,
 })
 
 const cartItemFromStorage = localStorage.getItem('cartItems') ?
-    JSON.parse(localStorage.getItem('cartItems')) : []
+JSON.parse(localStorage.getItem('cartItems')) : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
 
 const initialState = {
-    cart: { cartItems: cartItemFromStorage }
+    cart: { cartItems: cartItemFromStorage },
+    userInfo: {userInfo: userInfoFromStorage}
 }
 const middleware = [thunk]
 
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
 
 export default store
